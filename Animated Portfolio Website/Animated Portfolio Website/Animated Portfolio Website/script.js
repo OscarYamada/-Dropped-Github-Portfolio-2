854ba5f0edaf53731159f5e7608bbc5008b0c77e
Oscar Yamada <149902639+OscarYamada@users.noreply.github.com>
2024-09-05 04:53:19 +0000

    it's done. i've done it
    im so cooked

---
+++ Animated Portfolio Website/Animated Portfolio Website/Animated Portfolio Website/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Mechanical Engineer", "Resident Robotics Guy", "Coder and Builder"],
+    typeSpeed: 75,
+    backSpeed: 75,
+    backDelay: 1600,
+    loop: true
+})
